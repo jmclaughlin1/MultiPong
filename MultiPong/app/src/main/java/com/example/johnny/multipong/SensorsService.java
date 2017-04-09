53c1946f3b9ba565f2564e8b4739d2888b01ce39
Onkar <onkar.omr@gmail.com>
2017-04-09 15:33:02 -0400

    Added SensorsService Class

---
+++ MultiPong/app/src/main/java/com/example/johnny/multipong/SensorsService.java
@@ -0,0 +1,65 @@
+package com.example.johnny.multipong;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import static android.hardware.SensorManager.SENSOR_DELAY_NORMAL;
+
+/**
+ * This is the Service used for receiving the raw sensor values.
+ *
+ * Accelerometer_X  Gyroscope_X
+ * Accelerometer_Y  Gyroscope_Y
+ * Accelerometer_Z  Gyroscope_Z are the raw sensor values.
+ *
+ * Author: Onkar
+ */
+public class SensorsService {
+    SensorManager sensorManager;
+    Sensor sensor_gyro,sensor_acc;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor_gyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor_acc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(gyroListener, sensor_gyro,
+                SENSOR_DELAY_NORMAL);//SensorManager.SENSOR_DELAY_NORMAL
+        sensorManager.registerListener(accListener, sensor_acc,
+                SENSOR_DELAY_NORMAL);
+    }
+
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(gyroListener);
+        sensorManager.unregisterListener(accListener);
+    }
+
+    public SensorEventListener gyroListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor_gyro, int acc) { }
+
+        public void onSensorChanged(SensorEvent event1) {
+            float Gyroscope_X = event1.values[0];
+            float Gyroscope_Y = event1.values[1];
+            float Gyroscope_Z = event1.values[2];
+        }
+    };
+    public SensorEventListener accListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor_acc, int acc) { }
+
+        public void onSensorChanged(SensorEvent event2) {
+            float Accelerometer_X = event2.values[0];
+            float Accelerometer_Y = event2.values[1];
+            float Accelerometer_Z = event2.values[2];
+        }
+    };
+}
