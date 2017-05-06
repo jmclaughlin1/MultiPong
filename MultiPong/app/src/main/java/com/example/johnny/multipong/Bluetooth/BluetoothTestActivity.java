ee0c9ac2da3740a0bab4e6d40e3065abc21406b8
Jason Esquivel <jesquive90@gmail.com>
2017-05-06 13:20:55 -0400

    Turn on Bluetooth when waiting for device to connect

--- MultiPong/app/src/main/java/com/example/johnny/multipong/Bluetooth/BluetoothTestActivity.java
+++ MultiPong/app/src/main/java/com/example/johnny/multipong/Bluetooth/BluetoothTestActivity.java
@@ -1,8 +1,11 @@
 package com.example.johnny.multipong.Bluetooth;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.johnny.multipong.BaseActivity;
 import com.example.johnny.multipong.R;
@@ -12,18 +15,30 @@ import com.example.johnny.multipong.R;
  */
 
 public class BluetoothTestActivity extends BaseActivity {
+    // Intent request codes
+    private static final int REQUEST_ENABLE_BT = 2;
+
     // Return Intent extra
     public static String ACTION = "bluetooth_action";
     public static String ACTION_WAIT = "wait";
     public static String ACTION_CONNECT = "connect";
     public static String DEVICE_ADDRESS = "device_address"; // only sent when action is to connect
+
+    //Variable needed to start service
+    private String action;
+    private String address;
+
+    // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_test);
 
-        String action = getIntent().getExtras().getString(ACTION);
-        String address = "";
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        action = getIntent().getExtras().getString(ACTION);
+        address = "";
         if(action.equals(ACTION_CONNECT)){
             address = getIntent().getExtras().getString(DEVICE_ADDRESS);
         }
@@ -31,17 +46,47 @@ public class BluetoothTestActivity extends BaseActivity {
             address = "";
         }
 
-        Intent bluetoothServiceIntent = new Intent(this, BluetoothService.class);
-        bluetoothServiceIntent.putExtra(ACTION, action);
-        bluetoothServiceIntent.putExtra(DEVICE_ADDRESS, address);
 
-        startService(bluetoothServiceIntent);
 
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+        else {
+            Intent bluetoothServiceIntent = new Intent(this, BluetoothService.class);
+            bluetoothServiceIntent.putExtra(ACTION, action);
+            bluetoothServiceIntent.putExtra(DEVICE_ADDRESS, address);
+
+            startService(bluetoothServiceIntent);
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+                    Toast.makeText(this, "Bluetooth Enable", Toast.LENGTH_SHORT).show();
+                    Intent bluetoothServiceIntent = new Intent(this, BluetoothService.class);
+                    bluetoothServiceIntent.putExtra(ACTION, action);
+                    bluetoothServiceIntent.putExtra(DEVICE_ADDRESS, address);
+
+                    startService(bluetoothServiceIntent);
+                } else {
+                    // User did not enable Bluetooth or an error occured
+                    Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 
     @Override
